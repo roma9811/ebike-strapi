100283471e8e7295187461dc891ac2da958b618b
roma9811 <163286196+roma9811@users.noreply.github.com>
2025-05-23 14:02:07 +0200

    Настроил Cloudinary в плагинах загрузки

--- config/plugins.js
+++ config/plugins.js
@@ -7,4 +7,14 @@ module.exports = ({ env }) => ({
   'content-type-builder': {
     enabled: true,
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  },
 });
